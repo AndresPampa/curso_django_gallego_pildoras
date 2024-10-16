d2f27b517855231151f8e6dba276bfd2767a991a
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-10-16 01:08:15 -0300

    Terminado video 5, empezar por el 6

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 from datetime import *
 
 #Creamos una vista con Django --> PRimera vista
 def saludos(request):
-    documento = """<html><body>
-                   <h1>Hola Alumnos esta es nuestra primera pagina con Django</h1>
-                   </body></html>"""
+
+    doc_ext = open("C:/Users/Pampa/Desktop/Tecnicatura En Programacion/2024/16 - SegundoSemestre/Curso de Pildoras informaticas Django/curso_django_gallego_pildoras/Proyecto1/Proyecto1/plantillas/mi_plantilla.html")
+    plt = Template(doc_ext.read())
+    doc_ext.close()
+    ctx = Context()
+    documento = plt.render(ctx)
+
     return HttpResponse(f"{documento}")
 
 def despedida(request):
@@ -20,6 +25,7 @@ def dame_fecha(request):
     documento = f"""<html><body>
                    <h1>Fecha y Hora Actual: {fecha_actual}</h1>
                    </body></html>"""
+    
     return HttpResponse(documento)
 
 
