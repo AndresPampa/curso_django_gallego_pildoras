b32394d504ca54cb75261da71b2a7f6c49940b41
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-10-17 00:50:01 -0300

    Terminando video 7. EMpezar por el 8

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -2,13 +2,28 @@ from django.http import HttpResponse
 from django.template import Template, Context
 from datetime import *
 
+class Persona(object):
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
 #Creamos una vista con Django --> PRimera vista
 def saludos(request):
+
+    p1 = Persona("Profesor Juan", "Diaz")
+    # nombre = "Juan"
+    # apellido = "Diaz"
+    now = datetime.now()
+
+    temas_del_curso = []
 
     doc_ext = open("C:/Users/Pampa/Desktop/Tecnicatura En Programacion/2024/16 - SegundoSemestre/Curso de Pildoras informaticas Django/curso_django_gallego_pildoras/Proyecto1/Proyecto1/plantillas/mi_plantilla.html")
     plt = Template(doc_ext.read())
     doc_ext.close()
-    ctx = Context()
+    ctx = Context({"nombre_persona": p1.nombre, 
+                   "apellido_persona": p1.apellido, 
+                   "Hora": now, 
+                   "temas": temas_del_curso})#el contexto admite diccionarios
     documento = plt.render(ctx)
 
     return HttpResponse(f"{documento}")
