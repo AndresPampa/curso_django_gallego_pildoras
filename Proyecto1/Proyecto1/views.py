d890692b26a6c0909a09769e18d908c2303e8ec3
Andres Abdala Alvarez <andrespampa@gmail.com>
2024-10-25 23:32:20 -0300

    Terminando el video 9 y empezando con el 10

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import Template, Context
 from datetime import *
 from django.template import loader
+from django.shortcuts import render
 
 class Persona(object):
     def __init__(self, nombre, apellido):
@@ -21,7 +22,7 @@ def saludos(request):
     # doc_ext = open("C:/Users/Pampa/Desktop/Tecnicatura En Programacion/2024/16 - SegundoSemestre/Curso de Pildoras informaticas Django/curso_django_gallego_pildoras/Proyecto1/Proyecto1/plantillas/mi_plantilla.html")
     # doc_ext.close()
 
-    doc_ext = loader.get_template('mi_plantilla.html')
+    # doc_ext = loader.get_template('mi_plantilla.html')
 
     # plt = Template(doc_ext.read())
     # ctx = Context({"nombre_persona": p1.nombre, 
@@ -30,12 +31,9 @@ def saludos(request):
     #                "temas": temas_del_curso})#el contexto admite diccionarios
     # documento = plt.render(ctx)
     
-    documento = doc_ext.render({"nombre_persona": p1.nombre, 
-                   "apellido_persona": p1.apellido, 
-                   "Hora": now, 
-                   "temas": temas_del_curso})
+    # documento = doc_ext.render({"nombre_persona": p1.nombre, "apellido_persona": p1.apellido, "Hora": now, "temas": temas_del_curso})
 
-    return HttpResponse(f"{documento}")
+    return render(request, "mi_plantilla.html", {"nombre_persona": p1.nombre, "apellido_persona": p1.apellido, "Hora": now, "temas": temas_del_curso})
 
 def despedida(request):
     return HttpResponse("Hasta luego reyes")
